27f406b545316c7a0c8ddfb58602ebbbe4855d1b
Chris Kenward <chrisk@intersect.org.au>
2012-06-08 12:17:28 +1000

    Fix linux bug not being about get the active workbench window multiple times

--- exsite9/src/au/org/intersect/exsite9/commands/handlers/HandlerUtils.java
+++ exsite9/src/au/org/intersect/exsite9/commands/handlers/HandlerUtils.java
@@ -9,19 +9,18 @@ package au.org.intersect.exsite9.commands.handlers;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.activities.IActivityManager;
 import org.eclipse.ui.activities.IWorkbenchActivitySupport;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * Utillities used by the Handlers
  */
 public final class HandlerUtils
 {
-    public static void activateShowProjectActivity(final ExecutionEvent event)
+    public static void activateShowProjectActivity(final IWorkbenchWindow activeWorkbenchWindow)
     {
-        final IWorkbenchActivitySupport activitySupport = HandlerUtil.getActiveWorkbenchWindow(event).getWorkbench().getActivitySupport();
+        final IWorkbenchActivitySupport activitySupport = activeWorkbenchWindow.getWorkbench().getActivitySupport();
         final IActivityManager activityManager = activitySupport.getActivityManager();
         final Set<String> enabledActivities = new HashSet<String>();
         final String id = "au.org.intersect.exsite9.activity.showProject";
