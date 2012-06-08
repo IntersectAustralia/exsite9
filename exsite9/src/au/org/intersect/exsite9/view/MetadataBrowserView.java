b8e60d1852f47dd9c642d3b68ebf4ec6a64268f5
Daniel Yazbek <daniel@intersect.org.au>
2012-06-08 16:56:46 +1000

    Remove Eclipse RCP from cobertura statistics

--- exsite9/src/au/org/intersect/exsite9/view/MetadataBrowserView.java
+++ exsite9/src/au/org/intersect/exsite9/view/MetadataBrowserView.java
@@ -19,7 +19,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
