0451cab100df30b099301ab909920f02d9498727
Jake Farrell <jake@intersect.org.au>
2012-06-26 09:52:41 +1000

    EXSITE9-100: committing file that I missed in last commit

---
+++ exsite9/src/au/org/intersect/exsite9/commands/handlers/RenameGroupHandler.java
@@ -0,0 +1,103 @@
+package au.org.intersect.exsite9.commands.handlers;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import au.org.intersect.exsite9.domain.Group;
+import au.org.intersect.exsite9.service.IGroupService;
+
+public class RenameGroupHandler implements IHandler
+{
+
+    @Override
+    public void addHandlerListener(final IHandlerListener handlerListener)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void dispose()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException
+    {
+        final IStructuredSelection selection = (IStructuredSelection) HandlerUtil.getActiveWorkbenchWindow(event)
+                .getActivePage().getSelection();
+        final Object selectedObject = selection.getFirstElement();
+        
+        final IGroupService groupService = (IGroupService) PlatformUI.getWorkbench().getService(IGroupService.class);
+  
+        InputDialog userInput = new InputDialog(HandlerUtil.getActiveWorkbenchWindow(event).getShell(),
+                "Enter New Name", "Enter the name you wish to use", ((Group) selectedObject).getName(),
+                new IInputValidator()
+                {
+
+                    @Override
+                    public String isValid(String contents)
+                    {
+                        if (contents.trim().isEmpty())
+                        {
+                            return "Name must not be empty.";
+                        }
+
+                        if (contents.trim().length() >= 255)
+                        {
+                            return "Name is too long.";
+                        }
+
+                        // final String[] listOfValues = ((Group) selectedObject).;
+                        //
+                        // for (final String existingValue : listOfValues)
+                        // {
+                        // if (existingValue.equalsIgnoreCase(contents.trim()))
+                        // {
+                        // return "A Group with that name already exists at this level.";
+                        // }
+                        // }
+                        return null;
+                    }
+                });
+        userInput.open();
+
+        if (userInput.getValue() != null && !userInput.getValue().trim().isEmpty())
+        {
+            groupService.renameGroup(((Group) selectedObject), userInput.getValue().trim());
+            //((Group) selectedObject).setName(userInput.getValue().trim());
+        }
+
+        return ((Group) selectedObject);
+
+    }
+
+    @Override
+    public boolean isEnabled()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isHandled()
+    {
+        return true;
+    }
+
+    @Override
+    public void removeHandlerListener(final IHandlerListener handlerListener)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
